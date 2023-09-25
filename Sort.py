e62cae2ed47ddafbb97bb41c134f21408d48e691
rastogi17 <rastogikaustubh1586@gmail.com>
2023-09-25 23:46:51 +0530

    Abstract Sort class added again

---
+++ Sort.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+
+"""Module with the base implementation of a Sort class."""
+
+class Sort(ABC):
+    """Abstract base class for sorting."""
+
+    def __init__(self, items):
+        self._items = items
+
+    @abstractmethod
+    def _sort(self):
+        """Returns the sorted version of the elements contained
+        in the `_items` property.
+        Returns:
+            List: The sorted elements.
+        """
+        pass
+
+    def get_items(self):
+        return self._items
+
+    def _time(self):
+        self.time = 0
+        return self.time
+    
+"""Module with the implementation of the BubbleSort algorithm."""
+
+class BubbleSort(Sort):
+    """Class that represents a BubbleSort implementation."""
+
+    def _sort(self, items):
+        # your code here
+
+        return items
+
+    def _time(self, items):
+        # your code here
+
+        return self.time
+
+"""Module with the implementation of the MergeSort algorithm."""
+
+class MergeSort(Sort):
+    """Class that represents a MergeSort implementation."""
+
+    def _sort(self, items):
+        # your code here
+
+        return sorted_items
+
+    def _merge(self, left, right):
+        # your code here
+
+        return merged
+
+    def _time(self, items):
+        # your code here
+
+        return self.time
