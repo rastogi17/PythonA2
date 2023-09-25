4da68fcc87cbcb0f4191296b18328ad7be816cf9
cefiljoseph <137064270+cefiljoseph@users.noreply.github.com>
2023-09-26 00:13:09 +0530

    LinSearch Added

--- Search.py
+++ Search.py
@@ -22,13 +22,14 @@ class Search(ABC):
     
 """Module with the implementation of the LinSearch algorithm."""
 
-class LinSearch(Sort):
+class LinSearch(Search):
     """Class that represents a LinSearch implementation."""
 
-    def _search(self, items):
-        # your code here
-
-        return items
+    def _search(self,items):
+        for i in range(len(self._items)):
+            if self._items[i] == self._target:
+                return i
+        return -1
 
     def _time(self, items):
         # your code here
