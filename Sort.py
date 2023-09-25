f9720808c39b6ee5cf12f9a15ddd8d9c75d14bfa
rastogi17 <rastogikaustubh1586@gmail.com>
2023-09-26 02:32:35 +0530

    added error handling

--- Sort.py
+++ Sort.py
@@ -30,13 +30,17 @@ class BubbleSort(Sort):
     """Class that represents a BubbleSort implementation."""
 
     def _sort(self):
-        n = len(self._items)
-        for i in range(n):
-            for j in range(0, n-i-1):
-                if self._items[j] > self._items[j+1]:
-                    self._items[j], self._items[j+1] = self._items[j+1], self._items[j]
-        return self._items
-
+        try:
+            n = len(self._items)
+            for i in range(n):
+                for j in range(0, n-i-1): #iterating through every bubble of two consecutive items
+                    if self._items[j] > self._items[j+1]: # comparing two items if greater first then swapping them
+                        self._items[j], self._items[j+1] = self._items[j+1], self._items[j]
+            return self._items
+        except Exception as E: # error handling
+            print(f"An error occured: {E}")
+            return None
+    
     def _time(self, items):
         # your code here
 
