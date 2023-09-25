f443c4ff11117d8e1d78c972b468157f460eef72
rastogi17 <rastogikaustubh1586@gmail.com>
2023-09-25 23:35:28 +0530

    Added abstract search class

---
+++ Search.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+"""Module with the base implementation of a Search class."""
+
+class Search(ABC):
+    """Abstract base class for searching."""
+
+    def __init__(self, items):
+        self._items = items
+
+    @abstractmethod
+    def _search(self):
+
+        pass
+
+    def get_items(self):
+        return self._items
+
+    def _time(self):
+        self.time = 0
+        return self.time
+    
+"""Module with the implementation of the LinSearch algorithm."""
+
+class LinSearch(Sort):
+    """Class that represents a LinSearch implementation."""
+
+    def _search(self, items):
+        # your code here
+
+        return items
+
+    def _time(self, items):
+        # your code here
+
+        return self.time
+
+"""Module with the implementation of the BinSearch algorithm."""
+
+class BinSearch(Sort):
+    """Class that represents a BinSearch implementation."""
+
+    def _search(self, items):
+        # your code here
+
+        return False
+
+    def _time(self, items):
+        # your code here
+
+        return self.time
