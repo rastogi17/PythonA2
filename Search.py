fbef724706f7deea46b9f43050c7347253db3c31
DSik12 <davang.sikand@gmail.com>
2023-09-26 08:34:26 +0530

    Added Merge Sort

--- Search.py
+++ Search.py
@@ -1,51 +1,120 @@
 from abc import ABC, abstractmethod
+import time
 
-"""Module with the base implementation of a Search class."""
+class SearchError(Exception):
+    """Custom exception for searching errors."""
+    pass
 
 class Search(ABC):
-    """Abstract base class for searching."""
+    """Abstract base class for searching algorithms."""
 
-    def __init__(self, items):
+    def __init__(self, items, target):
+        """Initialize a Search instance with a list of items and a target to search for.
+
+        Args:
+            items (list): The list of items to search within.
+            target: The target item to search for.
+        """
         self._items = items
+        self._target = target
 
     @abstractmethod
     def _search(self):
+        """Performs a search to find the target item.
 
+        This method should be implemented by concrete searching algorithms.
+
+        Returns:
+            int: The index of the target item if found, -1 otherwise.
+        """
         pass
 
     def get_items(self):
+        """Get the original list of items.
+
+        Returns:
+            list: The original list of items.
+        """
         return self._items
 
     def _time(self):
+        """Get the time taken to execute the searching algorithm.
+
+        Returns:
+            float: The time taken in seconds.
+        """
         self.time = 0
         return self.time
-    
-"""Module with the implementation of the LinSearch algorithm."""
 
-class LinSearch(Sort):
-    """Class that represents a LinSearch implementation."""
+class LinSearch(Search):
+    """Implementation of the Linear Search algorithm."""
 
-    def _search(self, items):
-        # your code here
+    def _search(self):
+        """Performs a linear search to find the target item.
 
-        return items
+        Returns:
+            int: The index of the target item if found, -1 otherwise.
+        """
+        try:
+            start_time = time.time()
+            items = self._items.copy()
 
-    def _time(self, items):
-        # your code here
+            for i, item in enumerate(items):
+                if item == self._target:
+                    end_time = time.time()
+                    self.time = end_time - start_time
+                    return i  # Return the index where the target was found
 
+            end_time = time.time()
+            self.time = end_time - start_time
+            return -1  # Target not found
+        except Exception as e:
+            raise SearchError(f"Error while searching: {str(e)}")
+
+    def _time(self):
+        """Get the time taken to execute the Linear Search.
+
+        Returns:
+            float: The time taken in seconds.
+        """
         return self.time
 
-"""Module with the implementation of the BinSearch algorithm."""
+class BinSearch(Search):
+    """Implementation of the Binary Search algorithm."""
 
-class BinSearch(Sort):
-    """Class that represents a BinSearch implementation."""
+    def _search(self):
+        """Performs a binary search to find the target item.
 
-    def _search(self, items):
-        # your code here
+        Returns:
+            int: The index of the target item if found, -1 otherwise.
+        """
+        try:
+            start_time = time.time()
+            items = self._items.copy()
 
-        return False
+            left, right = 0, len(items) - 1
 
-    def _time(self, items):
-        # your code here
+            while left <= right:
+                mid = (left + right) // 2
+                if items[mid] == self._target:
+                    end_time = time.time()
+                    self.time = end_time - start_time
+                    return mid  # Return the index where the target was found
+                elif items[mid] < self._target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
 
+            end_time = time.time()
+            self.time = end_time - start_time
+            return -1  # Target not found
+        except Exception as e:
+            raise SearchError(f"Error while searching: {str(e)}")
+
+    def _time(self):
+        """Get the time taken to execute the Binary Search.
+
+        Returns:
+            float: The time taken in seconds.
+        """
         return self.time
--- Sort.py
+++ Sort.py
@@ -1,60 +1,139 @@
 from abc import ABC, abstractmethod
+import time
 
-"""Module with the base implementation of a Sort class."""
+class SortError(Exception):
+    """Custom exception for sorting errors."""
+    pass
+
 
 class Sort(ABC):
-    """Abstract base class for sorting."""
+    """Abstract base class for sorting algorithms."""
 
     def __init__(self, items):
+        """Initialize a Sort instance with a list of items to be sorted.
+
+        Args:
+            items (list): The list of items to be sorted.
+        """
         self._items = items
 
     @abstractmethod
     def _sort(self):
-        """Returns the sorted version of the elements contained
-        in the `_items` property.
-        Returns: 
-            List: The sorted elements.
+        """Sorts the list of items in ascending order.
+
+        This method should be implemented by concrete sorting algorithms.
+
+        Returns:
+            list: The sorted list of items.
         """
         pass
 
     def get_items(self):
+        """Get the original list of items.
+
+        Returns:
+            list: The original list of items.
+        """
         return self._items
 
     def _time(self):
+        """Get the time taken to execute the sorting algorithm.
+
+        Returns:
+            float: The time taken in seconds.
+        """
         self.time = 0
         return self.time
-    
-"""Module with the implementation of the BubbleSort algorithm."""
 
 class BubbleSort(Sort):
-    """Class that represents a BubbleSort implementation."""
+    """Implementation of the Bubble Sort algorithm."""
 
-    def _sort(self, items):
-        # your code here
+    def _sort(self):
+        """Sorts the list of items using the Bubble Sort algorithm.
 
-        return items
+        Returns:
+            list: The sorted list of items.
+        """
+        try:
+            start_time = time.time()
+            items = self._items.copy()
 
-    def _time(self, items):
-        # your code here
+            n = len(items)
+            for i in range(n):
+                for j in range(0, n - i - 1):
+                    if items[j] > items[j + 1]:
+                        items[j], items[j + 1] = items[j + 1], items[j]
 
+            end_time = time.time()
+            self.time = end_time - start_time
+
+            return items
+        except Exception as e:
+            raise SortError(f"Error while sorting: {str(e)}")
+
+    def _time(self):
+        """Get the time taken to execute the Bubble Sort algorithm.
+
+        Returns:
+            float: The time taken in seconds.
+        """
         return self.time
 
-"""Module with the implementation of the MergeSort algorithm."""
-
 class MergeSort(Sort):
-    """Class that represents a MergeSort implementation."""
+    """Implementation of the Merge Sort algorithm."""
 
-    def _sort(self, items):
-        # your code here
+    def _sort(self):
+        """Sorts the list of items using the Merge Sort algorithm.
 
-        return sorted_items
+        Returns:
+            list: The sorted list of items.
+        """
+        try:
+            start_time = time.time()
+            items = self._items.copy()
 
-    def _merge(self, left, right):
-        # your code here
+            def merge_sort(arr):
+                if len(arr) <= 1:
+                    return arr
 
-        return merged
+                mid = len(arr) // 2
+                left = arr[:mid]
+                right = arr[mid:]
 
-    def _time(self, items):
-        # your code here
+                left = merge_sort(left)
+                right = merge_sort(right)
 
+                return merge(left, right)
+
+            def merge(left, right):
+                result = []
+                i = j = 0
+
+                while i < len(left) and j < len(right):
+                    if left[i] < right[j]:
+                        result.append(left[i])
+                        i += 1
+                    else:
+                        result.append(right[j])
+                        j += 1
+
+                result.extend(left[i:])
+                result.extend(right[j:])
+                return result
+
+            sorted_items = merge_sort(items)
+
+            end_time = time.time()
+            self.time = end_time - start_time
+
+            return sorted_items
+        except Exception as e:
+            raise SortError(f"Error while sorting: {str(e)}")
+
+    def _time(self):
+        """Get the time taken to execute the Merge Sort algorithm.
+
+        Returns:
+            float: The time taken in seconds.
+        """
         return self.time
